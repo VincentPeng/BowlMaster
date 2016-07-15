711350d8544e6e9fd52f5026796e061f0b8f7b6d
vincent <pk0119@gmail.com>
2016-07-14 23:01:55 -0700

    1. Create Bowling pin prefab, add rigid body. 2. Add Camera control to the main camera which lets the camera follow the ball to pins.

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControl : MonoBehaviour {
+
+	public Ball ball;
+
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start() {
+		offset = ball.transform.position - this.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update() {
+		if (ball.transform.position.z > -700.0f) {
+			follow();
+		}
+	}
+
+	void follow() {
+		this.transform.position = ball.transform.position - offset;
+	}
+}
