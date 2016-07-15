92cb8dfcbae409ffdd2ccb0312335170ddbbeff0
vincent <pk0119@gmail.com>
2016-07-14 21:40:19 -0700

    Add the ball, although it looks like football right now...

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+
+	// Use this for initialization
+	void Start() {
+		Rigidbody rb = gameObject.GetComponent<Rigidbody>();
+		rb.velocity = new Vector3(0, 0, -200);
+	}
+	
+	// Update is called once per frame
+	void Update() {
+	
+	}
+}
