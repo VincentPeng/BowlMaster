e8e7e6f4889150c12a69abf9bcebd7801c2a0044
vincent <pk0119@gmail.com>
2016-08-14 00:48:03 -0700

    Add Unit test, environment packages

---
+++ Assets/Editor/FailTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+
+namespace VincentUnityTest
+{
+	[TestFixture]
+	public class SampleTests {
+		[Test]
+		public void EqualTest() {
+			Assert.AreEqual(1, 2);
+		}
+	}
+}
